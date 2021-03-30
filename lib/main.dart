32a158d7c47e2daa0b5fdb8f6cdb4dc62c01e7fe
Dipak <dipak.prajapati@bacancy.com>
2021-03-30 17:50:52 +0530

    firebase  initializeApp added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth/home.dart';
 
 import 'login.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
