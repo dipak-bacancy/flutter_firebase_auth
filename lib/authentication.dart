37946d18ba5eb521695ed3252ce21025d72bc916
Dipak <dipak.prajapati@bacancy.com>
2021-03-30 16:44:41 +0530

    auth files added

---
+++ lib/authentication.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthenticationProvider {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  get user => _auth.currentUser;
+
+//SIGN UP METHOD
+  Future<String> signUp({String email, String password}) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  //SIGN IN METHODJ
+  Future<String> signIn({String email, String password}) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  //SIGN OUT METHOD
+  Future<void> signOut() async {
+    await _auth.signOut();
+
+    print('signout');
+  }
+}
+
+/* 
+    Facebook key hash  :      4juqxR/bfCEhOEDslBTwAePpz74=
+ */
---
+++ lib/login.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_firebase_auth/authentication.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.all(8.0),
+        children: <Widget>[
+          SizedBox(height: 80),
+          // logo
+          Column(
+            children: [
+              FlutterLogo(),
+              SizedBox(height: 50),
+              Text(
+                'Welcome back!',
+                style: TextStyle(fontSize: 24),
+              ),
+            ],
+          ),
+
+          SizedBox(
+            height: 50,
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: LoginForm(),
+          ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Row(
+                children: <Widget>[
+                  Text('New here',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    child: Text('Get Registered Now!!',
+                        style: TextStyle(fontSize: 20, color: Colors.blue)),
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildLogo() {
+    return Container(
+      height: 80,
+      width: 80,
+      // padding: EdgeInsets.only(top: 20),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Colors.blue),
+      child: Center(
+        child: Text(
+          "T",
+          style: TextStyle(color: Colors.white, fontSize: 60.0),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  LoginForm({Key key}) : super(key: key);
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String email;
+  String password;
+
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          // email
+          TextFormField(
+            // initialValue: 'Input text',
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.email_outlined),
+              labelText: 'Email',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  const Radius.circular(100.0),
+                ),
+              ),
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+            onSaved: (val) {
+              email = val;
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+
+          // password
+          TextFormField(
+            // initialValue: 'Input text',
+            decoration: InputDecoration(
+              labelText: 'Password',
+              prefixIcon: Icon(Icons.lock_outline),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  const Radius.circular(100.0),
+                ),
+              ),
+              suffixIcon: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+                child: Icon(
+                  _obscureText ? Icons.visibility_off : Icons.visibility,
+                ),
+              ),
+            ),
+            obscureText: _obscureText,
+            onSaved: (val) {
+              password = val;
+            },
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+
+          SizedBox(height: 10),
+
+          SizedBox(
+            height: 54,
+            width: 184,
+            child: RaisedButton(
+              onPressed: () {
+                // Respond to button press
+
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+
+                  _formKey.currentState.save();
+
+                  AuthenticationProvider()
+                      .signIn(email: email, password: password)
+                      .then((result) {
+                    if (result == null) {
+                      Navigator.pushReplacementNamed(context, '/');
+                    } else {
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                          result,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ));
+                    }
+                  });
+                }
+              },
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(24.0))),
+              color: Colors.blue[400],
+              textColor: Colors.white,
+              child: Text(
+                'Login',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
